eaae1f1fd32f83c5497cbd653c2a0cab48730fd3
GustoniaEagle <gustoniaeagle@gmail.com>
2015-07-25 14:11:09 +0100

    Made the Crystallizer function.

--- src/main/java/com/eagle/resonantreflux/tileEntities/function/TileEntityFluxCrystallizer.java
+++ src/main/java/com/eagle/resonantreflux/tileEntities/function/TileEntityFluxCrystallizer.java
@@ -1,7 +1,12 @@
 package com.eagle.resonantreflux.tileentities.function;
 
 import com.eagle.resonantreflux.tileentities.core.TileEntityRR;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.ISidedInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 
 /**
  * This class was created by GustoniaEagle.
@@ -13,29 +18,217 @@ import net.minecraft.nbt.NBTTagCompound;
  * <p/>
  * File created @ 24/07/2015, 19:45 GMT.
  */
-public class TileEntityFluxCrystallizer extends TileEntityRR
+public class TileEntityFluxCrystallizer extends TileEntityRR implements ISidedInventory
 {
+    private ItemStack[] inv = new ItemStack[2];
+    private int progress;
+
     public TileEntityFluxCrystallizer()
     {
-        //40000000 RF per Crystal (40 million), but can be lowered to 6666668 RF per with boost.
         super(40000000, 2048);
     }
 
     @Override
     public void updateEntity()
     {
-
+        {
+            if (progress < 40000000)
+            {
+                storage.modifyEnergyStored(-2000);
+                progress += 2000;
+                markDirty();
+            }
+            else
+            {
+                progress = 0;
+                setInventorySlotContents(1, new ItemStack(Items.diamond));
+                markDirty();
+            }
+        }
     }
 
     @Override
     public void readFromNBT(NBTTagCompound tagCompound)
     {
         super.readFromNBT(tagCompound);
+        this.progress = tagCompound.getInteger("Progress");
+        NBTTagList nbttaglist = tagCompound.getTagList("Items", 10);
+        this.inv = new ItemStack[this.getSizeInventory()];
+
+        for (int i = 0; i < nbttaglist.tagCount(); ++i)
+        {
+            NBTTagCompound tagCompound1 = nbttaglist.getCompoundTagAt(i);
+            int j = tagCompound1.getByte("Slot") & 255;
+
+            if (j >= 0 && j < this.inv.length)
+            {
+                this.inv[j] = ItemStack.loadItemStackFromNBT(tagCompound1);
+            }
+        }
     }
 
     @Override
     public void writeToNBT(NBTTagCompound tagCompound)
     {
         super.writeToNBT(tagCompound);
+        tagCompound.setInteger("Progress", progress);
+        NBTTagList nbttaglist = new NBTTagList();
+
+        for (int i = 0; i < this.inv.length; ++i)
+        {
+            if (this.inv[i] != null)
+            {
+                NBTTagCompound nbttagcompound1 = new NBTTagCompound();
+                nbttagcompound1.setByte("Slot", (byte) i);
+                this.inv[i].writeToNBT(nbttagcompound1);
+                nbttaglist.appendTag(nbttagcompound1);
+            }
+        }
+
+        tagCompound.setTag("Items", nbttaglist);
+    }
+
+    @Override
+    public int getSizeInventory()
+    {
+        return 64;
+    }
+
+    @Override
+    public ItemStack getStackInSlot(int slot)
+    {
+        return inv[slot];
+    }
+
+    @Override
+    public ItemStack decrStackSize(int slot, int amount)
+    {
+        if (this.inv[slot] != null)
+        {
+            ItemStack itemstack;
+
+            if (this.inv[slot].stackSize <= amount)
+            {
+                itemstack = this.inv[slot];
+                this.inv[slot] = null;
+                this.markDirty();
+                return itemstack;
+            }
+            else
+            {
+                itemstack = this.inv[slot].splitStack(amount);
+
+                if (this.inv[slot].stackSize == 0)
+                {
+                    this.inv[slot] = null;
+                }
+
+                this.markDirty();
+                return itemstack;
+            }
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    @Override
+    public ItemStack getStackInSlotOnClosing(int slot)
+    {
+        if (this.inv[slot] != null)
+        {
+            ItemStack itemstack = this.inv[slot];
+            this.inv[slot] = null;
+            return itemstack;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    @Override
+    public void setInventorySlotContents(int slot, ItemStack itemStack)
+    {
+        this.inv[slot] = itemStack;
+
+        if (itemStack != null && itemStack.stackSize > this.getInventoryStackLimit())
+        {
+            itemStack.stackSize = this.getInventoryStackLimit();
+        }
+
+        this.markDirty();
+    }
+
+    @Override
+    public String getInventoryName()
+    {
+        return "Flux Crystallizer";
+    }
+
+    @Override
+    public boolean hasCustomInventoryName()
+    {
+        return false;
+    }
+
+    @Override
+    public int getInventoryStackLimit()
+    {
+        return 64;
+    }
+
+    @Override
+    public boolean isUseableByPlayer(EntityPlayer player)
+    {
+        return this.worldObj.getTileEntity(this.xCoord, this.yCoord, this.zCoord) == this && player.getDistanceSq(
+                (double) this.xCoord + 0.5D, (double) this.yCoord + 0.5D, (double) this.zCoord + 0.5D) <= 64.0D;
+    }
+
+    @Override
+    public void openInventory()
+    {
+
+    }
+
+    @Override
+    public void closeInventory()
+    {
+
+    }
+
+    @Override
+    public boolean isItemValidForSlot(int slot, ItemStack itemStack)
+    {
+        return true;
+    }
+
+    @Override
+    public int[] getAccessibleSlotsFromSide(int side)
+    {
+        //Booster goes in slot 0, output comes from slot 1.
+        switch (side)
+        {
+            case 0:
+                return new int[]{1};
+            case 1:
+                return new int[]{1};
+            default:
+                return new int[]{0};
+        }
+    }
+
+    @Override
+    public boolean canInsertItem(int slot, ItemStack itemStack, int side)
+    {
+        return slot == 0;
+    }
+
+    @Override
+    public boolean canExtractItem(int slot, ItemStack itemStack, int side)
+    {
+        return slot == 1;
     }
 }
+
